628345f93ccfcad6ef04e0f17ccff6cd29447e0f
Rian-ferreira <95186478+Rian-ferreira@users.noreply.github.com>
2025-05-31 15:37:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(() => {
+        const foto = document.querySelector('.sobre-foto');
+        foto.classList.add('aparecer');
+    }, 350);
+});
